a85b433447db92fa581b55200c5641794658ac33
Vasylchenko Serhii <98521667+VasylchenkoSerhii@users.noreply.github.com>
2023-01-09 22:37:24 +0200

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { fetchCurrentUser } from 'redux/auth/operations';
-import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
+// import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
 import Layout from 'components/Layout/Layout';
 import Home from 'pages/Home/Home';
 import Register from 'pages/Register';
@@ -20,12 +20,9 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        {/* <Route path="/contacts" element={<Contacts />} /> */}
+        <Route path="/contacts" element={<Contacts />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <PrivateRoute>
-          <Contacts />
-        </PrivateRoute>
       </Route>
     </Routes>
   );
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,56 +1,57 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 const token = {
-    set(token) {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    },
-    unset() {
-        axios.defaults.headers.common.Authorization = "";
-    },
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
 };
 
-export const register = createAsyncThunk("auth/register", async credentials => {
-    try {
-        const { data } = await axios.post("/users/signup", credentials);
-        token.set(data.token);
-        return data;
-    } catch (error) {
-        console.log(error);
-    };
+export const register = createAsyncThunk('auth/register', async credentials => {
+  try {
+    const { data } = await axios.post('/users/signup', credentials);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-export const logIn = createAsyncThunk("auth/login", async credentials => {
-    try {
-        const { data } = await axios.post("/users/login", credentials);
-        token.set(data.token);
-        return data;
-    } catch (error) {
-        console.log(error);
-    };
+export const logIn = createAsyncThunk('auth/login', async credentials => {
+  try {
+    const { data } = await axios.post('/users/login', credentials);
+    token.set(data.token);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-export const logOut = createAsyncThunk("auth/logout", async () => {
-    try {
-        await axios.post("/users/logout");
-        token.unset();
-    } catch (error) {
-        console.log(error);
-    };
+export const logOut = createAsyncThunk('auth/logout', async () => {
+  try {
+    await axios.post('/users/logout');
+    token.unset();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-export const fetchCurrentUser = createAsyncThunk("auth/refresh",
-    async (_, { getState, rejectWithValue }) => {
-        const persistedToken = getState().auth.token;
-        if (persistedToken === null) return rejectWithValue();
-        token.set(persistedToken);
-        try {
-            const { data } = await axios.get("/users/current");
-            return data;
-        } catch (error) {
-            console.log(error);
-        };
+export const fetchCurrentUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, { getState, rejectWithValue }) => {
+    const persistedToken = getState().auth.token;
+    if (persistedToken === null) return rejectWithValue();
+    token.set(persistedToken);
+    try {
+      const { data } = await axios.get('/users/current');
+      return data;
+    } catch (error) {
+      console.log(error);
     }
+  }
 );
